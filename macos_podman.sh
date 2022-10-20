8ccb18832a907218812bbf65beefaeabe19f6054
Peter Guy <peter@peterguy.com>
2022-10-20 11:59:07 -0700

    refine the podman install process and use a separate machine for Sourcegraph

--- macos_podman.sh
+++ macos_podman.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+### helper functions
+do-or-die() {
+    local message="${1}"
+    local command="${2}"
+    shift 2
+    "${command}" "${@}" || {
+        echo "${message}" 1>&2
+        exit 1
+    }
+}
+
 ### installs Sourcegraph Admin docker-compose on macOS
 
 ### check for xcode commandline tools
@@ -8,21 +19,27 @@ xcll=$(xcode-select --print-path 2>&1)
     echo "Installing the Xcode command line tools"
     xcode-select --install
 }
+echo "Checking for xcode commandline tools..."
 xcll=$(xcode-select --print-path 2>&1)
 [ -d "${xcll}" ] || {
-    echo "IUnable to find the Xcode command line tools" 1>&2
+    echo "Unable to find the Xcode command line tools" 1>&2
     exit 1
 }
+echo "OK"
 
 ### check for Homebrew
 command -v brew 2>/dev/null 1>&2 || {
     echo "installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
+echo "Checking for Homebrew..."
 command -v brew 2>/dev/null 1>&2 || {
     echo "Homebrew not installed" 1>&2
     exit 1
 }
+echo "OK"
+
+echo "Checking tooling.."
 
 ### install the dependencies
 packages=(python3 podman git jq)
@@ -38,6 +55,7 @@ do
     ${brew_prefix}/bin/${package} --version &>/dev/null || not_installed+=("${package}")
 done
 [ ${#not_installed[@]} -gt 0 ] && {
+    echo "Installing ${not_installed[*]}..."
     brew install ${not_installed[@]}
     # because brew installs symbolic links to the cellar,
     # the current shell won't find executables in PATH unless PATH is re-asserted
@@ -49,18 +67,21 @@ missing=0
 for package in ${packages[@]}
 do
     ${brew_prefix}/bin/${package} --version &>/dev/null || {
-        echo "${pckage} is not installed" 1>&2
+        echo "${package} is not installed" 1>&2
         missing=$((missing + 1))
     }
 done
 [ ${missing} -gt 0 ] && exit 1
 
 # NOTE I am assuming that pip3 gets installed with python3
+# For example, on Debian it is a separate package - python3-pip
 command -v podman-compose &>/dev/null || pip3 install podman-compose
 command -v podman-compose &>/dev/null || {
     echo "podman-compose not installed" 1>&2
     exit 1
 }
+
+echo "OK"
 
 ### set up the sourcegraph admin machine, if not already set up
 sg_machine="sourcegraph-admin"
@@ -68,40 +89,74 @@ podman machine inspect "${sg_machine}" &>/dev/null || {
     # TODO confirm machine size estimates - going with the low side for now
     # because this install is targetted at a user's machine
     # TODO re-create the machine if the specs are differnt from expected
+    echo "Bringing up a VM for Sourcegraph..."
     podman machine init \
         --cpus 4 \
-        -- disk-size 200 \
+        --disk-size 200 \
         --memory 4096 \
         --rootful \
-        sourcegraph-admin
+        "${sg_machine}"
 }
+
 podman machine inspect "${sg_machine}" &>/dev/null || {
-    echo "failed to provision a podman machine" 1>&2
+    echo "failed to provision a VM for Sourcegraph" 1>&2
     exit 1
 }
-[[ $(podman machine inspect "${sg_machine}" | jq '.[0].State') = running ]] || {
+
+echo "OK"
+
+echo "Starting up the Sourcegraph VM..."
+
+[[ $(podman machine inspect "${sg_machine}" | jq -r '.[0].State') = running ]] || {
     podman machine start ${sg_machine}
 }
-[[ $(podman machine inspect "${sg_machine}" | jq '.[0].State') = running ]] || {
+[[ $(podman machine inspect "${sg_machine}" | jq -r '.[0].State') = running ]] || {
     echo "failed to start a podman machine" 1>&2
     exit 1
 }
 
-cd &>/dev/null || {
-    echo "unable to set up Sourcegraph in user's HOME directory" 1>&2
-    exit 1
-}
-git pull https://github.com/sourcegraph/deploy-sourcegraph-docker || {
-    echo "unable to set up Sourcegraph in user's HOME directory" 1>&2
-    exit 1
-}
-cd deploy-sourcegraph-docker/docker-compose || {
-    echo "unable to find the Sourcegraph docker-compose folder" 1>&2
-    exit 1
-}
-podman-compose up -d || {
-    echo "failed to start up sourcegraph admin" 1>&2
-    exit 1
-}
+echo "OK"
 
+echo "Downloading Sourcegraph..."
+
+do-or-die "unable to set up Sourcegraph in ${HOME}" \
+    pushd "${HOME}"
+[ -d deploy-sourcegraph-docker ] || \
+    do-or-die "unable to set up Sourcegraph in user's HOME directory" \
+        git clone https://github.com/sourcegraph/deploy-sourcegraph-docker
+
+do-or-die "unable to find the Sourcegraph docker-compose folder"\
+    pushd deploy-sourcegraph-docker/docker-compose
+
+echo "OK"
+
+# since we're using a non-default machine, need to find connection info for it
+# go for the unix socket first
+socket=$(podman machine inspect "${sg_machine}" | jq -r '.[0].ConnectionInfo.PodmanSocket.Path')
+
+# --url=unix://$socket
+
+# if the unix socket doesn't work, set up for ssh
+# (the podman commands seem to use ssh by default, for some reason)
+ssh_port=$(podman machine inspect "${sg_machine}" | jq -r '.[0].SSHConfig.Port')
+ssh_identity=$(podman machine inspect "${sg_machine}" | jq -r '.[0].SSHConfig.IdentityPath')
+
+# --url=ssh://root@localhost:${ssh_port}/run/podman/podman.sock --identity=${ssh_identity}
+# NOTE: don't appear to need the identity file
+
+# export the connetion info because `podman-compose --podman-args`` adds the args after the sub-command
+# instead of between `podman` and the sub-command
+# (`podman volume --url=...` instead of `podman --url=... volume`)
+export CONTAINER_HOST=unix://${socket}
+
+do-or-die "failed to start up sourcegraph admin" \
+    podman-compose --project-name="${sg_machine}" up -d
+
+echo "OK"
+
+echo "Waiting for Sourcegraph to be ready..."
+sleep 3
+echo "OK"
+
+echo "Launching Sourcegraph..."
 open http://localhost
